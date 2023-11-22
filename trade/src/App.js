23646cec7c32385efb6e5bd12a2702765079fbf8
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-11-22 01:57:59 -0700

    new

--- trade/src/App.js
+++ trade/src/App.js
@@ -1,18 +1,12 @@
-import IMG from './picture/image1-1.png'
-import style from './App.css'
+import React from 'react'
+import Page from './component/Page'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App-header">
-      <img src={IMG} alt='' className='App-logo'/>
-       <div className='text'>
-       <h1 className='first'>Website under maintenance</h1>
-       <p className='second'>Our site is currently undergoing scheduled maintenance.</p>
-       <p className='second'>We should be back shortly. Thank you for your patience.</p>
-       </div>
+    <div>
+      <Page/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
