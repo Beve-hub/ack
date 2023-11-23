4d411b540eca89f31920819614cfd88d34e57fce
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-11-22 19:16:29 -0700

    logo

--- trade/src/component/Page.jsx
+++ trade/src/component/Page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import IMG from '../picture/image1-1.png'
+import Logo from '../picture/logo2.png' 
 import styles from './Page.css'
 
 const Page = () => {
@@ -10,6 +11,7 @@ const Page = () => {
      <h1 className='first'>Website under maintenance</h1>
      <p className='second'>Our site is currently undergoing scheduled maintenance.</p>
      <p className='second'>We should be back shortly. Thank you for your patience.</p>
+     <img src={Logo} alt='' className='logo'/>
      </div>
   </div>
   )
